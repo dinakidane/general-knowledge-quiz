f088c5283d5a4d02fa4021a43e811098da9dd61f
dinakidane <dinakidane@hotmail.com>
2023-04-24 21:42:18 +0000

    add html content for homepage

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// Wait for the DOM to finish loading before running the game
+
+document.addEventListener("DOMContentLoaded", function()) {
+    let buttons = document.getElementsByTagName("button");
+}
+
