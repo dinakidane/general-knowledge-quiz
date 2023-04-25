d9d966b2bb87b7bc7dc70915e601845d11428f9d
dinakidane <dinakidane@hotmail.com>
2023-04-25 17:44:21 +0000

    add quiz qustions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
-// Wait for the DOM to finish loading before running the game
+/** Wait for the DOM to finish loading before running the game */ 
 
 document.addEventListener("DOMContentLoaded", function()) {
     let buttons = document.getElementsByTagName("button");
 }
+
 
